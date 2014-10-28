78bb86dcb2960c851fd9d61fd25b590c10612ddf
‘michaelzhhan1 <hanz@cs.cmu.edu>
2014-10-28 00:23:12 -0400

    finish runningJob

--- src/ha_MR/RunningJob.java
+++ src/ha_MR/RunningJob.java
@@ -7,6 +7,18 @@ package ha_MR;
  */
 public class RunningJob extends Job {
   
+  private void linkJobTracker(){
+    /*
+     * use socket to link to jobtracker and check the status of the job, jobtracker will write to the port periodically to update the job status
+     */
+  }
+  public RunningJob(JobConf jconf){
+    this.setJobID(jconf.getJobID());
+    this.setJobIP(jconf.getJobIP());
+    this.setJobName(jconf.getJobName());
+    this.setJobPort(jconf.getJobPort());
+    
+  }
   
 
 }

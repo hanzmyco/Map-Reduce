6fb5f16b7496122d6a55eb6735f376eedc86321b
‘michaelzhhan1 <hanz@cs.cmu.edu>
2014-11-07 13:18:06 -0500

    detail

--- src/ha/mapreduce/JobInProgress.java
+++ src/ha/mapreduce/JobInProgress.java
@@ -1,5 +1,24 @@
 package ha.mapreduce;
 
+import java.net.InetSocketAddress;
+import java.util.List;
+
 public class JobInProgress {
+  private JobConf jc;
+  public JobInProgress(JobConf jc){
+    this.jc=jc;
+  }
+  public void runJob(){
+    List<InetSocketAddress> slaveList=jc.getSlaves();
+    int mapperNum=slaveList.size()*jc.getMappersPerSlave();
+    int reduceNum=slaveList.size()*jc.getReducersPerSlave();
+    // generate the number of TaskTracker the task track will launch mapper task and reducer task
+    
+    
+    
+    
+    
+  }
+  
 
 }

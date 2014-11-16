be74356a5ba92776ce230ae15877fbba836bd228
Amos Ng <amosjyng@gmail.com>
2014-11-15 23:57:31 -0500

    Changed test classes

--- src/ha/testing/IO/Read.java
+++ src/ha/testing/IO/Read.java
@@ -1,22 +1,18 @@
 package ha.testing.IO;
 
-import ha.IO.MR_IO;
 import ha.IO.NameNodeInterface;
 import ha.mapreduce.JobConf;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class test_Filewrite {
-  
+public class Read {
   public static void main(String []args) throws NotBoundException, UnknownHostException, IOException{
     if (args.length != 1) {
-      System.out.println("USAGE: java ha.mapreduce.JobClient <conf file> ");
+      System.out.println("USAGE: java ha.testing.IO.Read <conf file> ");
       System.exit(0);
     }
 
@@ -30,24 +26,10 @@ public class test_Filewrite {
     
     // every datanode(slave) has a namenode stub 
     NameNodeInterface stub=(NameNodeInterface)registry.lookup("NameNode");
-    stub.loopupReplicaSlave(0, "fuck");
-    System.out.println("finished use namenode");
-    
-    
-    MR_IO io=new MR_IO(stub,0,conf);
-    InetSocketAddress this_node=conf.getDatanodes().get(0);
-    InetSocketAddress other_node=conf.getDatanodes().get(1);
-    String localfile="../test";
-    System.out.println(other_node.toString());
-    System.out.println(this_node.toString());
-    //io.acceptWriteReplica(other_node.getPort());
-    io.requestWriteReplica(other_node, localfile,1,3);
-    
-
-  
+    stub.open("hello.txt");
+    stub.write("hello.txt", conf.toString());
   }
   
-  
-  
+
 
 }
--- src/ha/testing/IO/Write.java
+++ src/ha/testing/IO/Write.java
@@ -1,20 +1,18 @@
 package ha.testing.IO;
 
-import ha.IO.MR_IO;
 import ha.IO.NameNodeInterface;
 import ha.mapreduce.JobConf;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class test_Fileread {
+public class Write {
   public static void main(String []args) throws NotBoundException, UnknownHostException, IOException{
     if (args.length != 1) {
-      System.out.println("USAGE: java ha.mapreduce.JobClient <conf file> ");
+      System.out.println("USAGE: java ha.testing.IO.Write <conf file> ");
       System.exit(0);
     }
 
@@ -28,23 +26,7 @@ public class test_Fileread {
     
     // every datanode(slave) has a namenode stub 
     NameNodeInterface stub=(NameNodeInterface)registry.lookup("NameNode");
-    stub.loopupReplicaSlave(0, "fuck");
-    System.out.println("finished use namenode");
     
-    
-    MR_IO io=new MR_IO(stub,0,conf);
-    InetSocketAddress this_node=conf.getDatanodes().get(0);
-    InetSocketAddress other_node=conf.getDatanodes().get(1);
-    
-    System.out.println(other_node.toString());
-    System.out.println(this_node.toString());
-    io.acceptWriteReplica(other_node.getPort());
-   
-    
-
-  
+    System.out.println(stub.read("hello.txt", 5, 10));
   }
-  
-
-
 }

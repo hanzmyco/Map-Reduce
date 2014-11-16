ef010d6203ec0dd9be093017a9f8c50326c9b1dd
‘michaelzhhan1 <hanz@cs.cmu.edu>
2014-11-16 00:07:38 -0500

    modify master

--- src/ha/testing/IO/Read.java
+++ src/ha/testing/IO/Read.java
@@ -10,7 +10,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Read {
-  public static void main(String []args) throws NotBoundException, UnknownHostException, IOException{
+  public static void main(String[] args) throws NotBoundException, UnknownHostException,
+          IOException {
     if (args.length != 1) {
       System.out.println("USAGE: java ha.testing.IO.Read <conf file> ");
       System.exit(0);
@@ -19,20 +20,16 @@ public class Read {
     JobConf conf = new JobConf(args[0]);
     System.out.println("[CLIENT] Setting up new job as such:");
     System.out.println(conf);
- 
-    
+
     Registry registry = LocateRegistry.getRegistry(conf.getRmiServer().getHostString(), conf
             .getRmiServer().getPort());
-    
-    // every datanode(slave) has a namenode stub 
-    NameNodeInterface stub=(NameNodeInterface)registry.lookup("NameNode");
 
+    // every datanode(slave) has a namenode stub
+    NameNodeInterface stub = (NameNodeInterface) registry.lookup("NameNode");
 
     stub.open("hello.txt");
     stub.write("hello.txt", conf.toString());
 
   }
-  
-
 
 }

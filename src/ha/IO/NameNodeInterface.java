bb6afe3e1a481e02988a255620a9a5c780805bae
Amos Ng <amosjyng@gmail.com>
2014-11-15 22:54:01 -0500

    Added name node interface

--- src/ha/IO/NameNodeInterface.java
+++ src/ha/IO/NameNodeInterface.java
@@ -5,6 +5,15 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface NameNodeInterface extends Remote {
-  public InetSocketAddress loopupReplicaSlave(int slaveID,String filename)throws RemoteException;
-
+  public String read(String filename, int start, int end) throws RemoteException;
+  
+  public void write(String filename, String stuff) throws RemoteException;
+  
+  public void open(String filename) throws RemoteException;
+  
+  public void put(String filename, InetSocketAddress host) throws RemoteException;
+  
+  public Integer getFileSize(String filename) throws RemoteException;
+  
+  public void register(String rmiName) throws RemoteException;
 }
